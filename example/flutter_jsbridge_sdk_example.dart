08afc9ca91143e760c103ae286c11184885c48a6
xiebin <865068275@qq.com>
2022-12-20 16:38:03 +0800

    创建flutter_jsbridge_sdk工程

---
+++ example/flutter_jsbridge_sdk_example.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_jsbridge_sdk/flutter_jsbridge_sdk.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/flutter_jsbridge_sdk.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library flutter_jsbridge_sdk;
+
+export 'src/flutter_jsbridge_sdk_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
